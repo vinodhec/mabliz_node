8d53dfb637a8d40ce7725bdcb738737753e98511
Vinodh <vinodhec@gmail.com>
2023-01-07 13:01:16 +0530

    updates

--- src/models/Role.js
+++ src/models/Role.js
@@ -11,17 +11,29 @@ module.exports = (sequelize, DataTypes) => {
   Role.init(
     {
       name: DataTypes.STRING,
-      business_ids:{
+      business_type_label: DataTypes.STRING,
+      business_type: {
+        type: DataTypes.JSON,
+        set: function (val) {
+          this.setDataValue("business_type_label", val.label);
+
+          return this.setDataValue("business_type_id", val.id);
+        },
+      },
+      business_type_id: {
+        type: DataTypes.INTEGER,
+      },
+      branch_ids:{
         type: DataTypes.JSON,
         get: function () {
-          console.log(this.getDataValue("business_ids"));
-          return JSON.parse(this.getDataValue("business_ids") || "[]");
+          console.log(this.getDataValue("branch_ids"));
+          return JSON.parse(this.getDataValue("branch_ids") || "{}");
         },
         set: function (value) {
           // this.setDataValue('open_timing',value[0].time[0].start_time);
           // this.setDataValue('open_timing',value[0].time[value[0].time.length-1].end_time);
 
-          this.setDataValue("business_ids", JSON.stringify(value || []));
+          this.setDataValue("branch_ids", JSON.stringify(value || {}));
         },
 
       }
